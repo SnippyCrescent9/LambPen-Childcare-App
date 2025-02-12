c41f2490e67ac7bf483c9316c1c67a111b860e5d
Adam <adamsannes418@gmailcom>
2025-02-11 21:29:15 -0800

    First commit

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-lg my-10">Welcome to LambPen!</Text>
+      <Link href="/sign-in">Sign in</Link>
+    </View>
+  );
+}
